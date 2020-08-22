cc09cfd0b24079686177056b0c70ea46dff16459
Sam Bowering <samjbowering@gmail.com>
2020-08-22 18:03:48 +1200

    Save - clock not working

--- client/components/Clock.jsx
+++ client/components/Clock.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 class Clock extends React.Component {
   state = {
-    hours: 4, // RESET TO 0 - CURRENT VALUES ARE LAST HOLDERS
-    minutes: 6, // RESET TO 0 - CURRENT VALUES ARE LAST HOLDERS
+    hours: 4, // RESET TO 0 - CURRENT VALUES ARE PLACE HOLDERS
+    minutes: 6, // RESET TO 0 - CURRENT VALUES ARE PLACE HOLDERS
     timeActive: false
   }
 
@@ -23,8 +23,21 @@ class Clock extends React.Component {
         break
     }
 
+    function tick () {
+      if (this.state.minutes < 60) {
+        this.setState({
+          minutes: this.state.minutes + 1
+        })
+      } else if (this.state.hours < 24) {
+        this.setState({
+          hours: this.state.hours + 1,
+          minutes: 0
+        })
+      }
+    }
+
     while (this.state.timeActive === true) {
-      
+      setTimeout(tick, 1000)
     }
   }
 
@@ -36,7 +49,7 @@ class Clock extends React.Component {
     return (
       <>
         <h2>{this.state.hours}:{this.state.minutes}</h2>
-        <button>Start / Stop</button>
+        <button onClick={this.handleStartTime}>Start / Stop</button>
       </>
     )
   }
