64feb21a52c374b7a84c0275ea65b334ad7d7544
Sam Bowering <samjbowering@gmail.com>
2020-08-22 17:38:35 +1200

    Save

--- client/components/Clock.jsx
+++ client/components/Clock.jsx
@@ -3,11 +3,29 @@ import React from 'react'
 class Clock extends React.Component {
   state = {
     hours: 4, // RESET TO 0 - CURRENT VALUES ARE LAST HOLDERS
-    minutes: 6 // RESET TO 0 - CURRENT VALUES ARE LAST HOLDERS
+    minutes: 6, // RESET TO 0 - CURRENT VALUES ARE LAST HOLDERS
+    timeActive: false
   }
 
   handleStartTime = () => {
-    
+    switch (this.state.timeActive) {
+      case false:
+        this.setState({
+          timeActive: true
+        })
+        break
+      case true:
+        this.setState({
+          timeActive: false
+        })
+        break
+      default:
+        break
+    }
+
+    while (this.state.timeActive === true) {
+      
+    }
   }
 
   handleConvertToDisplay = () => {
